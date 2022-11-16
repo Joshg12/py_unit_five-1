61f196fa63fbc1b55b00787e8f2f53855d667c33
Josh <goldsjo@ssfs.org>
2022-11-16 08:57:58 -0500

    ee

--- fibonacci.py
+++ fibonacci.py
@@ -4,3 +4,20 @@ def fibonacci(x):
     :param number: The number of Fibonacci terms to return
     :return: A string consisting of a number of terms of the Fibonacci sequence.
     """
+
+
+def fib(number):
+    a = 1
+    b = 2
+    list_of_numbers = "1 1 "
+    for x in range(number):
+        c = a + b
+        list_of_numbers += str(c) + " "
+        a = b
+        b = c
+    print(list_of_numbers)
+
+
+
+fib(50)
+
