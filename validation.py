6e62e394a4eec9839396eb30510ef41fba6170ba
Josh <goldsjo@ssfs.org>
2022-12-01 11:45:26 -0500

    sadf

--- validation.py
+++ validation.py
@@ -1,13 +1,12 @@
 def get_input():
-    """
-    This function ensures that a user correctly enters in a number in the proper range.
-    :return: a value between 1 and 10, inclusive
-    """
-    pass  # make sure to delete this line after you complete your function
-
+   while True:
+    number = input("Enter a number fron 1-10")
+    if int(number) >=1 and int(number) <= 10:
+        return number
 
 def main():
     print(get_input())
+
 
 
 if __name__ == '__main__':
