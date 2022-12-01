17c27a2658b2d0da86d7845a2601f545893d7938
Josh <goldsjo@ssfs.org>
2022-12-01 11:12:26 -0500

    Update average.py

---
+++ average.py
@@ -0,0 +1,10 @@
+
+numbers = [1, 2, 34, 56, 7, 23, 23, 12, 1, 2, 3, 34, 56]
+sum = 0
+count = 0
+for number in numbers:
+    sum += number
+    count += 1
+average = sum / count
+print("The list of numbers is:", numbers)
+print("The average of all the numbers is:", average)
