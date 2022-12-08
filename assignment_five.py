cec76eb740cd3ed4aa7f3afbc575bfbdb02e67f3
Josh <goldsjo@ssfs.org>
2022-12-08 11:53:08 -0500

    Commit

--- assignment_five.py
+++ assignment_five.py
@@ -1,26 +1,24 @@
 print("Welcome to the guessing game?")
 input("What is your name?")
 
-
-
 import random
 
-random = (random.randint(0,9))
-print (random)
-
-
-def guess():
+def get_number():
+    randomn = random.randint(0,9)
+    print (randomn)
+    return randomn
+get_number()
+def get_guess():
     guess = int(input("Enter your guess"))
     return guess
-guess = guess()
-
 
 
 while True:
-    if  random == guess:
+    guess = get_guess()
+    randomn = get_number()
+    if random == guess:
         print("You guessed correctly!")
         break
     else: print("You guessed incorrectly, try again.")
-    guess()
-    if guess > random: print("The correct number is less than your guess.")
-    if guess < random: print("The correct number is greater than your guess.")
+    if guess > randomn: print("The correct number is less than your guess.")
+    if guess < randomn: print("The correct number is greater than your guess.")
