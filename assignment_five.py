c207a71ab23b2685e8f32f2feb3c41b4e256bef1
Josh <goldsjo@ssfs.org>
2022-12-09 13:39:15 -0500

    Commit

--- assignment_five.py
+++ assignment_five.py
@@ -7,18 +7,20 @@ def get_number():
     randomn = random.randint(0,9)
     print (randomn)
     return randomn
-get_number()
 def get_guess():
     guess = int(input("Enter your guess"))
     return guess
 
 
 while True:
-    guess = get_guess()
     randomn = get_number()
-    if random == guess:
-        print("You guessed correctly!")
-        break
-    else: print("You guessed incorrectly, try again.")
-    if guess > randomn: print("The correct number is less than your guess.")
-    if guess < randomn: print("The correct number is greater than your guess.")
+    while True:
+        guess = get_guess()
+
+        if randomn == guess:
+            print("You guessed correctly!")
+            break
+        else:
+            print("You guessed incorrectly, try again.")
+        if guess > randomn: print("The correct number is less than your guess.")
+        if guess < randomn: print("The correct number is greater than your guess.")
